6ae1d434f5a75966b874dba9f41aa2feaef73527
Tondwani <craigmangaladzi@gmail.com>
2025-04-28 21:02:06 +0200

    pushing my first migrations

--- BackEnd/Mveledziso/10.0.1/aspnet-core/src/Mveledziso.EntityFrameworkCore/Migrations/20250428185039_Initial migratiom.Designer.cs
+++ BackEnd/Mveledziso/10.0.1/aspnet-core/src/Mveledziso.EntityFrameworkCore/Migrations/20250428185039_Initial migratiom.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mveledziso.EntityFrameworkCore;
 
@@ -11,9 +12,11 @@ using Mveledziso.EntityFrameworkCore;
 namespace Mveledziso.Migrations
 {
     [DbContext(typeof(MveledzisoDbContext))]
-    partial class MveledzisoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250428185039_Initial migratiom")]
+    partial class Initialmigratiom
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
