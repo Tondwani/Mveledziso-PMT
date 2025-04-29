6e085008ceb3785a632eb65dbfbc59e65c5af0b1
Tondwani <craigmangaladzi@gmail.com>
2025-04-29 16:45:27 +0200

    Added the userDutyAppService

---
+++ BackEnd/Mveledziso/10.0.1/aspnet-core/src/Mveledziso.Application/Services/UserDutiesService/Dto/GetUserDutyInput.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mveledziso.Services.UserDutiesService.Dto
+{
+    public class GetUserDutyInput : PagedResultRequestDto
+    {
+        public long? UserId { get; set; }
+        public Guid? ProjectDutyId { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+    }
+}
---
+++ BackEnd/Mveledziso/10.0.1/aspnet-core/src/Mveledziso.Application/Services/UserDutiesService/UserDutyAppService.cs
@@ -0,0 +1,36 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using Abp.Linq.Extensions;
+using Mveledziso.Domain.Entities;
+using Mveledziso.Services.UserDutiesService.Dto;
+using System;
+using System.Linq;
+
+namespace Mveledziso.Services.UserDutiesService
+{
+    public class UserDutyAppService : AsyncCrudAppService<UserDuty, UserDutyDto, Guid, GetUserDutyInput, CreateUserDutyDto, UpdateUserDutyDto>, IUserDutyAppService
+    {
+        private readonly IRepository<UserDuty, Guid> _userDutyRepository;
+
+        public UserDutyAppService(IRepository<UserDuty, Guid> userDutyRepository)
+            : base(userDutyRepository)
+        {
+            _userDutyRepository = userDutyRepository;
+
+        }
+
+        protected override IQueryable<UserDuty> CreateFilteredQuery(GetUserDutyInput input)
+        {
+            return _userDutyRepository.GetAll()
+                .WhereIf(input.UserId.HasValue, x => x.UserId == input.UserId.Value)
+                .WhereIf(input.ProjectDutyId.HasValue, x => x.ProjectDutyId == input.ProjectDutyId.Value)
+                .WhereIf(input.FromDate.HasValue, x => x.CreationTime >= input.FromDate.Value)
+                .WhereIf(input.ToDate.HasValue, x => x.CreationTime <= input.ToDate.Value.AddDays(1).AddTicks(-1));
+        }
+
+        protected override IQueryable<UserDuty> ApplySorting(IQueryable<UserDuty> query, GetUserDutyInput input)
+        {
+            return query.OrderByDescending(x => x.CreationTime);
+        }
+    }
+}
