df127e401b160c5d2e576cf71ab780da557b8f5b
Tondwani <craigmangaladzi@gmail.com>
2025-04-29 12:15:08 +0200

    Enhancing the relationship of the system

---
+++ BackEnd/Mveledziso/10.0.1/aspnet-core/src/Mveledziso.Core/Domain/Entities/TimelinePhase.cs
@@ -0,0 +1,33 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mveledziso.Domain.Entities
+{
+    [Table("TimelinePhases")]
+    public class TimelinePhase : FullAuditedEntity<Guid>
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public Guid TimelineId { get; set; }
+
+        [ForeignKey("TimelineId")]
+        public virtual Timeline Timeline { get; set; }
+
+        public TimelinePhase()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
