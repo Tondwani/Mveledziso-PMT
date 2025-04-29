65a593400ccbaf0a856f00498f8da7558dc11f15
Tondwani <craigmangaladzi@gmail.com>
2025-04-29 13:07:36 +0200

    added the notification entity

---
+++ BackEnd/Mveledziso/10.0.1/aspnet-core/src/Mveledziso.Core/Domain/Entities/Notification.cs
@@ -0,0 +1,38 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mveledziso.Domain.Enums;
+
+namespace Mveledziso.Domain.Entities
+{
+    [Table("Notifications")]
+    public class Notification : CreationAuditedEntity<Guid>
+    {
+        [Required]
+        [StringLength(200)]
+        public string Message { get; set; }
+
+        public NotificationType Type { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public long UserId { get; set; } // Recipient
+
+        // Reference to the related entity
+        public string EntityType { get; set; }
+
+        public Guid EntityId { get; set; }
+
+        public Notification()
+        {
+            Id = Guid.NewGuid();
+            IsRead = false;
+        }
+    }
+
+}
