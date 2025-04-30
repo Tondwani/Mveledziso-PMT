ec24af3251018e3023fb6c757cf5dcca1285ffae
Tondwani <craigmangaladzi@gmail.com>
2025-04-30 14:48:46 +0200

    added a teamAppService

---
+++ aspnet-core/src/Mveledziso.Application/Services/TeamService/Dto/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Mveledziso.Domain.Entities;
+
+
+namespace Mveledziso.Services.TeamService.Dto
+{
+    public class TeamAppServiceMappingProfile : Profile
+    {
+
+        public TeamAppServiceMappingProfile()
+        { 
+
+        CreateMap<Team, TeamDto>()
+            .ForMember(dto => dto.ProjectCount, opt => opt.MapFrom(src => src.Projects.Count));
+
+        CreateMap<CreateTeamDto, Team>();
+        CreateMap<UpdateTeamDto, Team>();
+
+        }
+    }
+}
