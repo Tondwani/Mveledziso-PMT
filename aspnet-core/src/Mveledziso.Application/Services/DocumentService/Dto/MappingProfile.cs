35224b1247ca6c8008b5f7c1ee8eacfd7689b646
Tondwani <craigmangaladzi@gmail.com>
2025-04-30 14:41:17 +0200

    added a documentService

---
+++ aspnet-core/src/Mveledziso.Application/Services/DocumentService/Dto/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Mveledziso.Domain.Entities;
+
+namespace Mveledziso.Services.DocumentService.Dto
+{
+    public class DocumentMappingProfile : Profile
+    {
+        public DocumentMappingProfile()
+        {
+            CreateMap<Document, DocumentDto>();
+            CreateMap<CreateDocumentDto, Document>();
+            CreateMap<UpdateDocumentDto, Document>();
+        }
+    }
+}
