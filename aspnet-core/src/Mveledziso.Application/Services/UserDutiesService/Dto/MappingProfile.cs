0baa15daca86e2e0c04e3f577b20bfa652ce956c
Tondwani <craigmangaladzi@gmail.com>
2025-04-30 14:14:07 +0200

    Fixing a bug UserDutyAppService

---
+++ aspnet-core/src/Mveledziso.Application/Services/UserDutiesService/Dto/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Mveledziso.Domain.Entities;
+
+namespace Mveledziso.Services.UserDutiesService.Dto
+{
+    public class UserDutyMappingProfile : Profile
+    {
+        public UserDutyMappingProfile()
+        {
+            CreateMap<UserDuty, UserDutyDto>();
+            CreateMap<CreateUserDutyDto, UserDuty>();
+            CreateMap<UpdateUserDutyDto, UserDuty>();
+        }
+    }
+}
