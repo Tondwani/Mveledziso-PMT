e07f0dd03396a280b782c3a55875d558aeccec64
Tondwani <craigmangaladzi@gmail.com>
2025-05-01 10:05:54 +0200

    Add ActivitylogService

---
+++ aspnet-core/src/Mveledziso.Application/Services/ActivitylogService/Dto/ActivityLogListInputDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mveledziso.Services.ActivitylogService.Dto
+{
+    public class ActivityLogListInputDto
+    {
+        public long? UserId { get; set; }
+        public string Action { get; set; }
+        public string EntityType { get; set; }
+        public Guid? EntityId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int SkipCount { get; set; }
+        public int MaxResultCount { get; set; } = 10;
+    }
+}
