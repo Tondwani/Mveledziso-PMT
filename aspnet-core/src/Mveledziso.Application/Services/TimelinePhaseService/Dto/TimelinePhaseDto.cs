6da84d33f5fc1cd0d5a4a41f750ba5e60e495898
Tondwani <86742735+Tondwani@users.noreply.github.com>
2025-05-01 09:48:21 +0200

    Update TimelinePhaseDto.cs

--- aspnet-core/src/Mveledziso.Application/Services/TimelinePhaseService/Dto/TimelinePhaseDto.cs
+++ aspnet-core/src/Mveledziso.Application/Services/TimelinePhaseService/Dto/TimelinePhaseDto.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Mveledziso.Services.TimelinePhaseService.Dto
 {
+    [AutoMapFrom(typeof(TimelinePhase))]
     public class TimelinePhaseDto
     {
         public Guid Id { get; set; }
